c047a7bfcac17aeb60564b92ecddc05b55672b92
Daria <Daria.Osipova.V@gmail.com>
2016-09-14 18:29:50 +0300

    Improved functionality of the calculator by enabling it to save and display the initial operation as well.

--- calc.py
+++ calc.py
@@ -1,44 +1,47 @@
 class Calc:
 
-	def __init__(self):
-		self.total = 0
-		self.memory = []
-		
+ def __init__(self):
+  self.total = 0
+  self.memory = [] 
+  self.init_expression = []
 
-	def add(self, a, b = None):	
-		if b is None: 
-			self.total += a
-			self.memory.append(' + {0}'.format(a))
-		
-		else:
-			self.total = a + b 
-			self.memory = [] 
-		
-		
-	def subtract(self, a, b = None):
-		if b is None:
-			self.total -= a 
-			self.memory.append(' - {0}'.format(a))
-		else:
-			self.total = a - b
-			self.memory = []
-			
-	def multiply(self, a, b = None):
-		if b is None:
-			self.total *= a
-			self.memory.append(' * {0}'.format(a))
-		else:
-			self.total = a * b
-			self.memory = []
-			
-	def divide(self, a, b = None):
-		if b is None:
-			self.total /= a
-			self.memory.append(' / {0}'.format(a))
-		else:
-			self.total = a / b 
-			self.memory = []
-			
-	def show_memory(self): 
-		for index in range(len(self.memory)):
-			print(index,': ',self.memory[index])
+ def add(self, a, b = None): 
+  if b is None: 
+   self.total += a
+   self.memory.append(' + {0}'.format(a))
+  else:
+   self.total = a + b 
+   self.memory = []
+   self.init_expression.append('{0} + {1}'.format(a, b)) 
+  
+ def subtract(self, a, b = None):
+  if b is None:
+   self.total -= a 
+   self.memory.append(' - {0}'.format(a))
+  else:
+   self.total = a - b
+   self.memory = []
+   self.init_expression.append('{0} - {1}'.format(a, b))
+   
+ def multiply(self, a, b = None):
+  if b is None:
+   self.total *= a
+   self.memory.append(' * {0}'.format(a))
+  else:
+   self.total = a * b
+   self.memory = []
+   self.init_expression.append('{0} * {1}'.format(a, b))
+   
+ def divide(self, a, b = None):
+  if b is None:
+   self.total /= a
+   self.memory.append(' / {0}'.format(a))
+  else:
+   self.total = a / b 
+   self.memory = []
+   self.init_expression.append('{0} / {1}'.format(a, b))   
+   
+ def show_memory(self): 
+  operation_list = self.init_expression + self.memory
+  for index in range(len(operation_list)):
+   print(index,': ',operation_list[index])
